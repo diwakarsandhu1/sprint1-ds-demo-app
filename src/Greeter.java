20e077a6bb14242be9b90ceef33b30f9f61e5dc7
Jimbo James <jimbo@james.com>
2026-08-19 16:19:05 -0400

    add farewell

--- src/Greeter.java
+++ src/Greeter.java
@@ -2,4 +2,9 @@ public class Greeter {
     public String greet(String name) {
         return "Hello, " + name;
     }
+
+    public String farewell(String name) {
+        return "Goodbye, " + name;
+    }
+}
 }
